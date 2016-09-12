781f086ded002e6df5c31294b94c9bc0e92b2c56
wacleva <wacleva@hotmail.com>
2016-09-12 13:39:54 +0800

    add chapter GUI

--- MaterialTest/js/question.js
+++ MaterialTest/js/question.js
@@ -2,10 +2,14 @@
   'use strict';
   angular
       .module('questionss',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-      .controller('qnCtrl', function() {
+      .controller('qnCtrl', function($scope) {
         this.chosenChapter = '';
         this.chapters = ('Chapter 1  Chapter 2  Chapter 3').split('  ').map(function (chapter) { return { abbrev: chapter }; });
         this.chosenQnType = '';
         this.qnTypes = ('MCQ  Spreadsheet  Video  Slides').split('  ').map(function (qnType) { return { abbrev: qnType }; });
+
+      $scope.addChapter ={
+
+      }
       });
 })();
